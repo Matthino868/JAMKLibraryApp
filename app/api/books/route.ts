507a2546b7f6a89613930d2a94dfe144f78f8cfa
Matthino868 <matthino868@gmail.com>
2024-12-08 18:52:15 +0200

    Updated some settings

--- app/api/books/route.ts
+++ app/api/books/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse, NextRequest } from 'next/server';
-import prisma from '@/lib/prisma';
+import prisma from '../../../lib/prisma';
 import { getServerSession } from 'next-auth/next'; // Import NextAuth's getServerSession
 
 // GET: Fetch books from the database by Id or query (Unprotected)
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import './globals.css';
 import { ReactNode } from 'react';
-import Provider from '@/context/Provider';
+import Provider from '../context/Provider';
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
